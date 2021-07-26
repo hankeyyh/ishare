43417160754befcd5ead25782d3af2dd18f8b648
hankeyyh <406078328@qq.com>
2021-07-26 23:53:43 +0800

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
 # coding: utf-8
 from albumy import create_app
+from dotenv import load_dotenv
+import os
 
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+	load_dotenv(dotenv_path)
+	
 app = create_app('pub')
